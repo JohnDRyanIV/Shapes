c852be04712e08e115ba7b014e335ee54b207a70
JohnDRyanIV <43046528+JohnDRyanIV@users.noreply.github.com>
2020-09-04 21:21:33 -0700

    First commit, skeleton of project

---
+++ shapes/src/test/java/tests/AllTests.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestRectangleLogic.class, TestSquareLogic.class })
+public class AllTests {
+
+}
